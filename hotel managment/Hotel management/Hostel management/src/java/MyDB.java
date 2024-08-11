255a60426c3ec03cfbeca112915ff6bb1e4481e4
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-11 13:38:09 +0530

    Updated  getLogger

--- hotel managment/Hotel management/Hostel management/src/java/MyDB.java
+++ hotel managment/Hotel management/Hostel management/src/java/MyDB.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,10 +6,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Oops
- */
+
 public class MyDB {
     Connection con;
     public Connection getCon(){
