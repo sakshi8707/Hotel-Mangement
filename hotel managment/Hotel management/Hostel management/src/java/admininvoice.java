90082fb7e46bfab3b9f7995e84d6a84de8138a04
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-16 16:26:16 +0530

    Added processRequest

--- hotel managment/Hotel management/Hostel management/src/java/admininvoice.java
+++ hotel managment/Hotel management/Hostel management/src/java/admininvoice.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,21 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Oops
- */
+
 public class admininvoice extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -81,11 +66,7 @@ public class admininvoice extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
