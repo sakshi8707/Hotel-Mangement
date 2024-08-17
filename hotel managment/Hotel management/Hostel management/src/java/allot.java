6ae8a0f6167bda0968beab0177a3d92e21c361f2
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-17 16:48:53 +0530

    Updated doGet

--- hotel managment/Hotel management/Hostel management/src/java/allot.java
+++ hotel managment/Hotel management/Hostel management/src/java/allot.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -22,15 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class allot extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -65,14 +53,7 @@ public class allot extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
